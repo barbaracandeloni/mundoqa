efae8efe7f2c6c78438c96bf7a1cf92fd0390376
Candeloni <93953502+barbaracandeloni@users.noreply.github.com>
2024-09-06 20:52:32 -0300

    Primeira versão do site.

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Testes de Caixa Preta",
+        descricao: "É uma técnica chave de Testes Funcionais. O usuário que está realizando esse teste não tem contato nenhum com código fonte do sistema. São técnicas de testes Caixa preta: Partição de Equivalência, Análise de Valores Limite  máximo, Testes de Caminho Básico e Testes de Tabela de Decisão.",
+        link: "https://glossary.istqb.org/pt_BR/search?search?term=&exact_matches_first=true&page=2",
+        tags: "analista E2E"
+    },
+    {
+        titulo: "Fundamentos dos Testes de Software",
+        descricao: "Os testes de software são um processo sistemático de verificação e validação de um produto de software para garantir que ele atenda aos requisitos especificados e funcione conforme o esperado. O objetivo principal é encontrar defeitos, garantir a qualidade, reduzir riscos e aumentar a confiança do usuário.",
+        link: "https://www.devmedia.com.br/guia/tecnicas-e-fundamentos-de-testes-de-software/34403",
+        tags: "iniciante"
+    },
+    {
+        titulo: "Testes Não Funcionais",
+        descricao: "Os testes não funcionais avaliam aspectos do software que não estão diretamente relacionados às suas funcionalidades, mas que são cruciais para a experiência do usuário e a qualidade do sistema. Alguns exemplos são: desempenho, segurança, usabilidade, confiabilidade.",
+        link: "https://blog.vericode.com.br/testes-nao-funcionais/",
+        tags: "performance carga"
+    }
+];
